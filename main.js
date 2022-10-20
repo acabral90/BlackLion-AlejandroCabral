bea3aaf2d2bd5f5e1b2d88a5f99d9bdd51ca1f61
Alejandro Cabral <aomcabral@gmail.com>
2022-10-20 17:35:21 -0300

    segunda entrega proyecto final

--- main.js
+++ main.js
@@ -1,61 +1,25 @@
-//E-Comerce Black Lion Grow Shop
-
-//primero declaro el constructor
 class Producto {
-    constructor(nombre, precio) {
-        this.nombre = nombre; 
-        this.precio = precio;
-    }
+	constructor(nombre, precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
 }
 
-//luego declaro el array donde se almacenaran los productos que elijo
+const productosCarrito = [];
 
-const productosCarrito = []; 
+const carrito = () => {
+	return productosCarrito.reduce((acumulador, producto) => acumulador + producto.precio,0);
+};
 
-//creo una funcion en donde creo los productos y los almaceno en el array ya mencionado
-
-const agregarCarrito = () => {
-    let nombre = prompt("Que producto deseas?");
-    let precio = parseFloat(prompt("Indique el valor del producto"));
-
-    let productoAgregado = new Producto (nombre, precio);
-
-    productosCarrito.push(productoAgregado);
-
-    let seguirAgregando = prompt("Desea agregar mas productos al carrito de compras?");
-
-    if(seguirAgregando == "si"){
-        agregarCarrito();
-        
-        if(seguirAgregando == "no"){
-            carrito();
-        }
-
-        else{
-            alert("Debe indicar si desea o no seguir agregando productos al carrito");
-        }
-    }
+let confirmacion = confirm("¿Deseas agregar un producto?");
+while (confirmacion) {
+	let nombre = prompt("Que producto deseas agregar?");
+	let precio = parseFloat(prompt("Indique el valor del producto"));
+	let productoAgregado = new Producto(nombre, precio);
+	productosCarrito.push(productoAgregado);
+	confirmacion = confirm("¿Deseas agregar otro producto?");
 }
 
-//creo una funcion para sumar los precios de losproductos
-
-const carrito = ()=>{
-    const precios = productosCarrito.map(function(prod){
-        alert(prod.precio)
-    });
-
-    let suma = 0;
-
-    for(const total of precios){
-        suma += total;
-        alert(suma);
-    }
-
-}
-
-
-agregarCarrito();
-
-carrito();
+alert(carrito());
 
 
